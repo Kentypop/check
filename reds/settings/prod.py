eb8262f172a78ed448aa86648c3d40221ce82fc5
seant <Levimcc7@hotmail.com>
2021-05-01 19:25:39 +0900

    changed url herokuapp

--- reds/settings/prod.py
+++ reds/settings/prod.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'desolate-citadel-45217.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'redsjb.herokuapp.com']
 
 LOGGING ={
     'version':1,
